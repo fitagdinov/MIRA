f7f3a8f435bdfd6f0703c139fd0a3838a27c0f7f
molozey <lipskii.as@phystech.edu>
2023-05-22 02:23:59 +0300

    SQL: добавлена таблица для создания красивых слов для ввода бабушками

--- testDbService/test_initialize.py
+++ testDbService/test_initialize.py
@@ -1,13 +1,13 @@
 import unittest
 from dbService import get_request
 
-TABLES_NEED_TO_BE = {('memberEmbedding',), ('EventGroupMap',), ('StaticEvent',), ('memberStatic',), ('StaticGroup',), ('EventEmbedding',), ('AttendanceGroup',), ('DynamicGroup',)}
+TABLES_NEED_TO_BE = {('memberEmbedding',), ('EventGroupMap',), ('StaticEvent',), ('StaticMember',), ('StaticGroup',), ('EventEmbedding',), ('AttendanceGroup',), ('DynamicGroup',), ('StaticCiteEventID',)}
 
 
 class TestCorrectInitialize(unittest.TestCase):
     def test_AllTablesExist(self):
         tables_in_db = set(get_request("SHOW TABLES", execute_many=True))
-        self.assertEqual(tables_in_db, TABLES_NEED_TO_BE)
+        self.assertTrue(tables_in_db == TABLES_NEED_TO_BE)
 
 
 if __name__ == '__main__':
