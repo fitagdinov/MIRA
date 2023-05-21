1ca1ea1e7ef9521b89d094366f737d1d5da3f7e5
dmik-id <dmitriev.i@phystech.edu>
2023-05-21 18:14:23 +0300

    Navibar v_1.0

---
+++ .history/front/src/App_20230521152536.js
@@ -0,0 +1,12 @@
+import Navibar from "./components/Navibar";
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+      <Navibar/>
+    </>
+  );
+}
+
+export default App;
---
+++ .history/front/src/components/Navibar_20230521152117.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Button, Navbar, Button, Link} from 'react-bootstrap'
+import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
+
+
+export default function Navibar (){
+    <>
+        <Navbar collapseOnSelect expand='lg' bg='dark' variant="dark">
+            <Navbar.Brand>Привет Бабка</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="mr-auto">
+                <Nav className='mr-auto'>
+                    <Nav.Lik>1</Nav.Lik>
+                    <Nav.Lik>2</Nav.Lik>
+                    <Nav.Lik>3</Nav.Lik>                    
+                </Nav>
+                <Nav>
+                    <Button variant="primary">вход</Button>
+                    <Button variant="primary">выход</Button>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </>
+}
