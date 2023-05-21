ce132988e8688d562bab97c8918338218d1b9a35
dmik-id <dmitriev.i@phystech.edu>
2023-05-21 20:55:36 +0300

    opros v_1.0

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,19 @@
 import Navibar from "./components/Navibar";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route, Link} from 'react-router-dom'
+import Opros from "./pages/Opros";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <Navibar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/opros" element={<Opros />}/>
+
+      </Routes>
+      {/* <Opros/> */}
     </>
   );
 }
--- front/src/pages/Opros.js
+++ front/src/pages/Opros.js
@@ -1,12 +1,30 @@
-import React, {useState} from 'react';
-
+import React from 'react';
+import { Container, Row, Col, Card, Button, Form} from 'react-bootstrap';
+import babka from '../image/images.jpeg'
 const Opros = () => {
-
+ 
     return (
-        <div>
-            Opros
-        </div>
+        <Container>
+            <Form>
+                <Row>
+                    <Form.Label>вопрос 1</Form.Label>
+                    <Form.Label>вопрос 2</Form.Label>
+                    <Form.Label>вопрос 3</Form.Label>
+                    
+                    <Col>
+                        <Card style={{width: "18rem"}}>
+                            <Card.Img variant='top' src={babka}/>
+                            <Card.Body>
+                                <Card.Title>первое мероприятие</Card.Title>
+                                <Card.Text>walk</Card.Text>
+                                <Button variant='primary'>Выбрать</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Form>
+        </Container>
     );
 };
 
-export default Admin;
+export default Opros;
