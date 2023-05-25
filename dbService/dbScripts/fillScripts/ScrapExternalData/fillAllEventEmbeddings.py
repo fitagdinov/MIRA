9a8ebe580169b3dc5f5e6f0ce4e0422b1bdc2328
molozey <lipskii.as@phystech.edu>
2023-05-25 12:03:56 +0300

    fix merge errors

--- dbService/dbScripts/fillScripts/ScrapExternalData/fillAllEventEmbeddings.py
+++ dbService/dbScripts/fillScripts/ScrapExternalData/fillAllEventEmbeddings.py
@@ -1,13 +1,11 @@
 from dbService.dbScripts import addEventEmbeddingScript
 import pandas as pd
 from tqdm import tqdm
-from ZeroMallocFix import validate_memory_location
 
 if __name__ == '__main__':
     df = pd.read_csv('../fileStorage/EventEmbedding.csv')
 
     for line in tqdm(range(0, df.shape[0])):
-        validate_memory_location()
         addEventEmbeddingScript(
             event_id=df["EXTERNAL_ID_event"].values,
             embedding=df["event_embedding_vector"].values
