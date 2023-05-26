f4d38d5ee201d9e6f95629f38120f3c5a7d3d183
mrFendel <novikov.ivan@phystech.edu>
2023-05-26 13:06:53 +0300

    i hate windows

--- dbService/dbScripts/fillScripts/ScrapExternalData/fillAllGroups.py
+++ dbService/dbScripts/fillScripts/ScrapExternalData/fillAllGroups.py
@@ -1,3 +1,7 @@
+import time
+
+import mysql.connector.errors
+
 from dbService.dbScripts import addGroupScript
 import pandas as pd
 from numpy import any
@@ -25,12 +29,19 @@ if __name__ == '__main__':
         # validate_memory_location()
         _tmp = df.iloc[line]
         _rasp = 'Активные:' + str(_tmp['расписание в активных периодах']) + '[-]Закрытые:' + str(_tmp['расписание в закрытых периодах']) + '[-]Плановые:' + str(_tmp['расписание в плановом периоде'])
-        addGroupScript(
-            external_id_group=_tmp["уникальный номер"],
-            group_short_name=_tmp["shorten_name"],
-            online_status=_tmp["online_status"],
-            group_address=_tmp["адрес площадки"],
-            group_geo=_tmp["округ площадки"],
-            group_area=_tmp["район площадки"],
-            group_schedule=_rasp
-        )
+        successStatus = False
+        while not successStatus:
+            try:
+                addGroupScript(
+                    external_id_group=_tmp["уникальный номер"],
+                    group_short_name=_tmp["shorten_name"],
+                    online_status=_tmp["online_status"],
+                    group_address=_tmp["адрес площадки"],
+                    group_geo=_tmp["округ площадки"],
+                    group_area=_tmp["район площадки"],
+                    group_schedule=_rasp
+                )
+                successStatus = True
+            except Exception as e:
+                print(e)
+                time.sleep(1)
