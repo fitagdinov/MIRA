a37a285e131c034772614893e62f15a1d36edb52
mrFendel <novikov.ivan@phystech.edu>
2023-05-26 09:15:02 +0300

    restored normal version

--- front/src/components/Navibar.js
+++ front/src/components/Navibar.js
@@ -1,10 +1,10 @@
 import React, {useState } from "react";
 import {Navbar, Button, Nav, Modal, Form} from 'react-bootstrap'
 import "../styles/Form.css"
-import {FaSearch, FaHistory, FaHeart, FaEye} from "react-icons/fa";
-
+import {FaSearch, FaHistory, FaHeart, FaEye, FaHome} from "react-icons/fa";
 import { authUser } from "../action/auth";
 import { useDispatch, useSelector } from 'react-redux';
+import logo from '../image/Logo.svg'
 
 
 export default function Navibar (){
@@ -12,9 +12,9 @@ export default function Navibar (){
     const [show, setShow] = useState(false) // хранит состояни модального окна авторизации
     const [fio, setFio] = useState(101387414) // хранит в себе состояние ввыода ФИО
     const [birthDate, setBirthDate] = useState('1937-02-17') // хранит в себе состояние ввыода даты рождения
-    const [showOpros, setShowOpros] = useState(false) // хранит в себе состояние модального окна с просьбой пройти опрос 
-    const dispatch = useDispatch() 
-    const auth = useSelector(state => state.authTest) // важно чтоб было authTest - название редюсера 
+    const [showOpros, setShowOpros] = useState(false) // хранит в себе состояние модального окна с просьбой пройти опрос
+    const dispatch = useDispatch()
+    const auth = useSelector(state => state.authTest) // важно чтоб было authTest - название редюсера
     const handleClose = () => setShow(false) //закрывает окно авторизации
     const handleShow = () => setShow(true) // открывает окно авторизации
     const handleCloseOpros = () => setShowOpros(false) // закрывает окно "пройдите опрос"
@@ -28,22 +28,25 @@ export default function Navibar (){
     if (auth.grand_exist == true){
         return(
             <>
-                <Navbar collapseOnSelect expand='lg' bg='dark' variant="dark">
-                    <Navbar.Brand className="navbar-brand px-3"> Привет,{auth.grand_address}  </Navbar.Brand>
+                <Navbar style={{fontSize: '20px'}} collapseOnSelect expand='lg' bg='dark' variant="dark">
+                    <Navbar.Brand className="navbar-brand px-3">
+                        <img style={{height: '50px'}} src={logo} alt={'Logo'}/> {auth.grand_address}
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className='mx-auto'>
+                            <Nav.Link href={'/'}><FaHome/> Главная</Nav.Link>
                             <Nav.Link href={'/recomendation'}><FaEye/> Рекомендации</Nav.Link>
                             <Nav.Link><FaSearch/> Расширеный поиск</Nav.Link>
-                            <Nav.Link><FaHistory/> История</Nav.Link>
+                            <Nav.Link><FaHistory/> Мои мероприятия </Nav.Link>
                             <Nav.Link><FaHeart/> Избранное</Nav.Link>
                         </Nav>
                         <Nav className="px-3">
-                            <Button variant="primary" className="mr-2" onClick={handleShow}> Авторизация </Button>
+                            <Button size={'lg'} variant="primary" className="mr-2" onClick={handleShow}> Авторизация </Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-        
+
                 {/*TODO: при введении данных должна случиться авторизация (прохождение опроса д.б. необязательным)*/}
                 <Modal size={'lg'} show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
@@ -51,73 +54,76 @@ export default function Navibar (){
                             Пожалуйста введите свое ФИО и дату рождения для авторизации
                         </Modal.Title>
                     </Modal.Header>
-        
+
                     <Modal.Body>
                         <Form>
                             <Form.Group controlId="fromBasicEmail">
                                 <Form.Label>ФИО</Form.Label>
-                                <Form.Control 
-                                    className={'name'} 
+                                <Form.Control
+                                    className={'name'}
                                     placeholder="ФИО"
                                     value={fio}
                                     onChange={(e) => setFio(e.target.value)}/>
-        
+
                             </Form.Group>
                             <Form.Group controlId="fromBasicEmail">
                                 <Form.Label>Дата рождения</Form.Label>
-                                <Form.Control 
-                                className={'date'} 
-                                placeholder="Дата рождения"
-                                value={birthDate}
-                                onChange={(e) => setBirthDate(e.target.value)}/>
+                                <Form.Control
+                                    className={'date'}
+                                    placeholder="Дата рождения"
+                                    value={birthDate}
+                                    onChange={(e) => setBirthDate(e.target.value)}/>
                             </Form.Group>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer >
-                        <Button 
+                        <Button
                             className={'mx-auto'}
-                            variant="primary" 
-                            onClick={() => {handleShowOpros(fio, birthDate);}} // при нажатии вызываем функцию происходит действи (описано выше)                                           
-                            >
-                                Готово</Button>
+                            variant="primary"
+                            onClick={() => {handleShowOpros(fio, birthDate);}} // при нажатии вызываем функцию происходит действи (описано выше)
+                        >
+                            Готово</Button>
                     </Modal.Footer>
                 </Modal>
                 <Modal size={'lg'} show={showOpros} onHide={handleCloseOpros}>
                     <Modal.Header closeButton>
                         <Modal.Title>
-                        Ответьте на 3 вопроса, чтобы мы могли предложить Вам действительно интересные мероприятия.
+                            Ответьте на 3 вопроса, чтобы мы могли предложить Вам действительно интересные мероприятия.
                         </Modal.Title>
                     </Modal.Header>
-        
+
                     <Modal.Footer className={'text-center'}>
                         <Form className={'p-3'}>
                             <Button className={'mx-auto'} href={'/opros/1'}>Начать</Button>
                         </Form>
                     </Modal.Footer>
-        
+
                 </Modal>
             </>
-            )
+        )
     }
     else {
         return(
             <>
-                <Navbar collapseOnSelect expand='lg' bg='dark' variant="dark">
-                    <Navbar.Brand className="navbar-brand px-3"> Привет,{auth.grand_address}  </Navbar.Brand>
+                <Navbar style={{fontSize: '20px'}} collapseOnSelect expand='lg' bg='dark' variant="dark">
+                    <Navbar.Brand className="navbar-brand px-3">
+                        <img style={{height: '50px'}} src={logo} alt={'Logo'}/> {auth.grand_address}
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className='mx-auto'>
+                            <Nav.Link href={'/'}><FaHome/> Главная</Nav.Link>
                             <Nav.Link href={'/recomendation'}><FaEye/> Рекомендации</Nav.Link>
-                            <Nav.Link><FaSearch/> Расширеный поиск</Nav.Link>
-                            {/* <Nav.Link><FaHistory/> История</Nav.Link> */}
-                            {/* <Nav.Link><FaHeart/> Избранное</Nav.Link> */}
+                            <Nav.Link><FaSearch/> Поиск</Nav.Link>
+                            {/*<Nav.Link><FaHistory/> Мои мероприятия</Nav.Link>*/}
+                            {/*<Nav.Link><FaHeart/> Избранное</Nav.Link>*/}
                         </Nav>
                         <Nav className="px-3">
-                            <Button variant="primary" className="mr-2" onClick={handleShow}> Авторизация </Button>
+                            <Button variant="primary" size={'lg'} className="mr-2" onClick={handleShow}> Авторизация </Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-        
+
                 {/*TODO: при введении данных должна случиться авторизация (прохождение опроса д.б. необязательным)*/}
                 <Modal size={'lg'} show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
@@ -125,52 +131,52 @@ export default function Navibar (){
                             Пожалуйста введите свое ФИО и дату рождения для авторизации
                         </Modal.Title>
                     </Modal.Header>
-        
+
                     <Modal.Body>
                         <Form>
                             <Form.Group controlId="fromBasicEmail">
                                 <Form.Label>ФИО</Form.Label>
-                                <Form.Control 
-                                    className={'name'} 
+                                <Form.Control
+                                    className={'name'}
                                     placeholder="ФИО"
                                     value={fio}
                                     onChange={(e) => setFio(e.target.value)}/>
-        
+
                             </Form.Group>
                             <Form.Group controlId="fromBasicEmail">
                                 <Form.Label>Дата рождения</Form.Label>
-                                <Form.Control 
-                                className={'date'} 
-                                placeholder="Дата рождения"
-                                value={birthDate}
-                                onChange={(e) => setBirthDate(e.target.value)}/>
+                                <Form.Control
+                                    className={'date'}
+                                    placeholder="Дата рождения"
+                                    value={birthDate}
+                                    onChange={(e) => setBirthDate(e.target.value)}/>
                             </Form.Group>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer >
-                        <Button 
+                        <Button
                             className={'mx-auto'}
-                            variant="primary" 
+                            variant="primary"
                             onClick={() => {handleShowOpros(fio, birthDate);}} // при нажатии вызываем функцию происходит действи (описано выше)                                           
-                            >
-                                Готово</Button>
+                        >
+                            Готово</Button>
                     </Modal.Footer>
                 </Modal>
                 <Modal size={'lg'} show={showOpros} onHide={handleCloseOpros}>
                     <Modal.Header closeButton>
                         <Modal.Title>
-                        Ответьте на 3 вопроса, чтобы мы могли предложить Вам действительно интересные мероприятия.
+                            Ответьте на 3 вопроса, чтобы мы могли предложить Вам действительно интересные мероприятия.
                         </Modal.Title>
                     </Modal.Header>
-        
+
                     <Modal.Footer className={'text-center'}>
                         <Form className={'p-3'}>
                             <Button className={'mx-auto'} href={'/opros/1'}>Начать</Button>
                         </Form>
                     </Modal.Footer>
-        
+
                 </Modal>
             </>
-            )
+        )
     }
 }
---
+++ front/src/pages/Test.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRepos } from '../action/repos';
+import { authUser } from '../action/auth';
+import {showEvents} from "../action/showEvents";
+
+
+const Test = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Test;
