3efc6d5f0de44dd5214ce64e0e3d66eae2545daa
Robert <robertfitagdinov@gmail.com>
2023-05-28 06:20:45 +0300

    utils - should work but don't

--- for_place/utils.py
+++ for_place/utils.py
@@ -23,32 +23,3 @@ print("Travel time in minutes:", route_travel_time/60)
 
 fig, ax = ox.plot_graph_route(G, route, node_size=0, figsize=(40,40),tiles='openstreetmap')
 print(time.time()-s)
-# station_st_node_id = ox.distance.nearest_nodes(G, [151.014898], [-34.06714])[0]
-# ox.config(log_console=True, use_cache=True)
-#
-# # define the start and end locations in latlng
-# start_latlng = (37.78497,-122.43327)
-# end_latlng = (37.78071,-122.41445)
-#
-# # location where you want to find your route
-
-#
-# # find shortest route based on the mode of travel
-# mode = 'walk' # 'drive', 'bike', 'walk'
-#
-# # find shortest path based on distance or time
-# optimizer = 'time' # 'length','time'
-#
-# # create graph from OSM within the boundaries of some
-# # geocodable place(s)
-# graph = ox.graph_from_place(place, network_type = mode)
-#
-# # find the nearest node to the start location
-# orig_node = ox.get_nearest_node(graph, start_latlng)
-#
-# # find the nearest node to the end location
-# dest_node = ox.get_nearest_node(graph, end_latlng)
-#
-# # find the shortest path
-# shortest_route = nx.shortest_path(graph, orig_node,dest_node,
-#                                   weight=optimizer)
