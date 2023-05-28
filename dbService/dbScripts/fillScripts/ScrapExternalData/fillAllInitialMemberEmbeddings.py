715fc5c6f30726fe731a8e2be296c766885a85f6
molozey <lipskii.as@phystech.edu>
2023-05-28 04:39:37 +0300

    init_emb adding error handling inf cycle

--- dbService/dbScripts/fillScripts/ScrapExternalData/fillAllInitialMemberEmbeddings.py
+++ dbService/dbScripts/fillScripts/ScrapExternalData/fillAllInitialMemberEmbeddings.py
@@ -1,3 +1,4 @@
+import time
 import warnings
 
 import numpy as np
@@ -7,6 +8,7 @@ import tqdm
 from dbService.dbConfiguration import get_request, put_request
 import json
 from dbService import embedding_size
+import mysql.connector
 
 if __name__ == '__main__':
     all_users = get_request(
@@ -31,37 +33,52 @@ if __name__ == '__main__':
     q = [pd.read_csv(path + f"q{i+1}.csv").values for i in range(number_of_questions)]
 
     for user_id in tqdm.tqdm(all_users):
-        _id = user_id[0]
-
-        init_emb = avg_emb
-
-        pool_was_passed = get_request(
-            query=f"""SELECT PollWasPassed FROM NoOldMen.DynamicPollMember WHERE SYS_ID_grand={_id}""",
-            execute_many=False
-        )[0]
-
-        if pool_was_passed:
-            events_json = get_request(
-                query=f"""SELECT selected_Events FROM NoOldMen.StaticPoolResult WHERE SYS_ID_grand={_id}""",
-                execute_many=False
-            )[0]
-
-            events = json.loads(events_json)
+        statusPut = False
+        while not statusPut:
             try:
-                mask_questions = [events[f"question{i+1}"] for i in range(number_of_questions)]
-            except KeyError:
-                warnings.warn("Old format pool keys. Unable to convert to mask")
-            for i in range(number_of_questions):
-                try:
-                    init_emb += (np.array(q[i]).T * np.array(mask_questions[i])).T.sum(axis=0)
-                except:
-                    continue
-        init_emb = str(init_emb.astype(float).tolist())[1:-1]
+                _id = user_id[0]
 
-        put_request(
-            query=f"""
-                    INSERT INTO NoOldMen.memberInitialEmbedding 
-                    VALUES ({_id},{init_emb});
-                    """
-        )
+                init_emb = avg_emb
+
+                pool_was_passed = get_request(
+                    query=f"""SELECT PollWasPassed FROM NoOldMen.DynamicPollMember WHERE SYS_ID_grand={_id}""",
+                    execute_many=False
+                )[0]
+
+                if pool_was_passed:
+                    events_json = get_request(
+                        query=f"""SELECT selected_Events FROM NoOldMen.StaticPoolResult WHERE SYS_ID_grand={_id}""",
+                        execute_many=False
+                    )[0]
+
+                    events = json.loads(events_json)
+                    try:
+                        mask_questions = [events[f"question{i+1}"] for i in range(number_of_questions)]
+                    except KeyError:
+                        warnings.warn("Old format pool keys. Unable to convert to mask")
+                    for i in range(number_of_questions):
+                        try:
+                            init_emb += (np.array(q[i]).T * np.array(mask_questions[i])).T.sum(axis=0)
+                        except:
+                            continue
+                init_emb = str(init_emb.astype(float).tolist())[1:-1]
+
+                put_request(
+                    query=f"""
+                            INSERT INTO NoOldMen.memberInitialEmbedding 
+                            VALUES ({_id},{init_emb});
+                            """
+                )
+                statusPut = True
+            except TimeoutError:
+                warnings.warn("TimeOutError")
+                time.sleep(1)
+            except mysql.connector.Error as e:
+                warnings.warn(f"MySQL error: {e}")
+                if e.errno == 2003:
+                    time.sleep(1)
+                elif e.errno == 1062:
+                    print("Cannot fill line caused by the worst OS system in the world. Loss data")
+                    statusPut = True
+                    continue
 
